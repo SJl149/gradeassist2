9f291a110c4f3173ee01601c248792f52cdc4799
ScottL <sjlteacher@gmail.com>
2018-02-04 21:26:15 -0500

    commented out drop semesters.

--- db/migrate/20180114144112_drop_semesters.rb
+++ db/migrate/20180114144112_drop_semesters.rb
@@ -1,12 +1,6 @@
 class DropSemesters < ActiveRecord::Migration
   def change
-    drop_table :semesters do |t|
-      t.string :name
-      t.datetime :start_date
-      t.datetime :end_date
-      t.integer :course_id
-      t.datetime :created_at, null: false
-      t.datetime :updated_at, null: false
-    end
+    # drop_table :semesters 
+
   end
 end
