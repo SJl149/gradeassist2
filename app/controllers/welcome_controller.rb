2959b12e0767c30f38710e186ab6ae02d85a162e
ScottL <sjlteacher@gmail.com>
2018-02-04 19:03:37 -0500

    Views cleanedup.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -13,20 +13,4 @@ class WelcomeController < ApplicationController
   def about
   end
 
-  def date_selector
-    class_days = []
-    @course.class_days.each { |class_day| class_days << class_day[:day_of_week] }
-    holidays = []
-    @course.holidays.each { |holiday| holidays << holiday.class_date.to_date }
-    date_selection = Date.today.to_date
-
-    if date_selection >= @course.start_date.to_date && date_selection <= @course.end_date.to_date
-      while class_days.exclude?(date_selection.wday) || holidays.include?(date_selection)
-        date_selection = date_selection.next_day(1)
-      end
-      date_selection
-    else
-      @course.start_date.to_date
-    end
-  end
 end
