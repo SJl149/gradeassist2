3b5478fea0195b45ef79e3ff5e9f096833fd3ae3
ScottL <sjlteacher@gmail.com>
2018-02-17 15:42:04 -0500

    Daily_grades created and persisted properly.

--- app/models/daily_grade.rb
+++ app/models/daily_grade.rb
@@ -1,3 +1,7 @@
 class DailyGrade < ActiveRecord::Base
   belongs_to :student
+
+  def self.find_or_create(class_date, category)
+    self.where(class_date: class_date.beginning_of_day..class_date.end_of_day, category: category).first || self.create(class_date: class_date, category: category)
+  end
 end
