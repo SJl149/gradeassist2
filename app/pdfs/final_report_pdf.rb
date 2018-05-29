af59ade6cc3ea1074818aae5da77cfb200e233b5
ScottL <sjlteacher@gmail.com>
2018-05-28 20:42:38 -0400

    Grade breakdown table working, but needs calculations of grades.

--- app/pdfs/final_report_pdf.rb
+++ app/pdfs/final_report_pdf.rb
@@ -2,8 +2,11 @@ class FinalReportPdf
   include Prawn::View
 
   def initialize(current_user, course)
+    @document = Prawn::Document.new(:page_layout => :landscape)
     @current_user = current_user
     @course = course
+    @students = @course.students.order(:family_name)
+    @categories = @course.categories
     title
     grades_table
   end
@@ -22,9 +25,41 @@ class FinalReportPdf
   end
 
   def grades_table_rows
-    [["Name", "Absences", "Class Participation", "Homework", "Quiz/Test", "Final Test/ Project", "Total Avg w/o Absences", "Total Avg w/ Absences"]] +
-    @course.students.map  do |student|
-      ["#{student.family_name}, #{student.given_name} (#{student.nickname})", "", "", "", "", "", "", ""]
+    [table_header] +
+    @students.map  do |student|
+      [student_line]
     end
+  end
+
+  def table_header
+    column_names = ["Name", "Absences"]
+    @categories.each { |cat| column_names << cat.name }
+    column_names << "Total Avg w/o Absences"
+    column_names << "Total Avg w/ Absences"
+    column_names
+  end
+
+  def student_line(student)
+    attendance = student_attendance(student)
+    line = ["#{student.family_name}, #{student.given_name} (#{student.nickname})", "#{attendance}"]
+    final_grade = 0
+    @categories.each do |category|
+      student_category_average = student.daily_grades.where(category: category.name).average(:grade).to_i
+      final_grade += student_category_average * (category.weight / 100.0)
+      line << student_category_average
+    end
+    line << final_grade.to_i
+    if attendance == 5
+      # reduce by one letter grade
+    elsif attendance >= 6
+      line << "F"
+    else
+      line << final_grade.to_i
+    end
+    line
+  end
+
+  def student_attendance(student)
+    student.attendances.where(status: 2).count + (student.attendances.where(status: 1).count / 3)
   end
 end
